5fe7c799d156ab1c4ecee0929ec96b8433ae364a
xuanhung1509 <vuxuanhung15092002@gmail.com>
2023-05-01 16:58:38 +0700

    chore: remove boilerplate, add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
