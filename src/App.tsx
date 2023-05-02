8db5c5803af63893197dde0de327bd751c243c1f
xuanhung1509 <vuxuanhung15092002@gmail.com>
2023-05-02 15:31:41 +0700

    fix: add app background color

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import { RatingCard } from './components/RatingCard'
 
 const App = () => {
   return (
-    <div className="mx-auto my-8 max-w-sm px-4">
-      <RatingCard />
+    <div className="h-screen w-full bg-slate-900">
+      <div className="flex min-h-[75vh] items-center justify-center">
+        <div className="mx-auto max-w-sm px-4 py-8">
+          <RatingCard />
+        </div>
+      </div>
     </div>
   )
 }
